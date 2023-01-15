ee37887ad7d95d2e74a8c313171e707c1406d332
MariTHH <118300117+MariTHH@users.noreply.github.com>
2023-01-15 12:10:03 +0300

    Update Gender.java

--- src/Gender.java
+++ src/Gender.java
@@ -2,6 +2,12 @@ public enum Gender {
     human("человек"),
     animal("животное");
     private String NameGender;
-    Gender(String name){this.NameGender=name;}
-    public String getNameGender(){return NameGender;}
+
+    Gender(String name) {
+        this.NameGender = name;
+    }
+
+    public String getNameGender() {
+        return NameGender;
+    }
 }
