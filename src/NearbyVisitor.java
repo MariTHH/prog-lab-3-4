13e70cf854ae62f350d32fba27071e3702e192e7
MariTHH <118300117+MariTHH@users.noreply.github.com>
2023-01-15 12:12:53 +0300

    Update NearbyVisitor.java

--- src/NearbyVisitor.java
+++ src/NearbyVisitor.java
@@ -1,10 +1,11 @@
-public class NearbyVisitor extends Character{
+public class NearbyVisitor extends Character {
 
-    public NearbyVisitor(String name, int health, Location location, Gender gender, Personality personality) {
-        super(name, health, location, gender, personality);
+    public NearbyVisitor() {
+        super("Ближстоящий", 100, Location.floor, Gender.human, Personality.calm);
     }
-    public String fire(){
-        this.health = health - Math.round(Math.random()*35);
+
+    public String fire() {
+        this.health = health - Math.round(Math.random() * 35);
         return " у " + name + " полыхнуло огнем";
     }
 }
