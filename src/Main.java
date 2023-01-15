99a5bc565b875503f18c3db09f564e8bd824cccf
MariTHH <118300117+MariTHH@users.noreply.github.com>
2023-01-15 12:12:11 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,33 @@
-public class Main{
-    public static void main(String[] args){
-        Cat cat = new Cat("Кот",100,Location.mantel, Gender.animal, Personality.calm);
-        cat.drop(Items.primus,cat);
-        cat.getLocation();
-        Thrower thrower = new Thrower("Бросавший", 100, Location.floor, Gender.human, Personality.calm);
+public class Main {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.drop(Items.primus, cat);
+        cat.getLocation(Location.floor);
+        Thrower thrower = new Thrower();
         thrower.drop(Items.net, cat);
         cat.drink(Items.petrol);
         cat.toCatch(Items.browning, Location.outofback);
-        NearbyVisitor nearbyVisitor = new NearbyVisitor("ближстоящий",100,Location.floor, Gender.human,Personality.calm);
-        cat.target(Items.browning,nearbyVisitor);
+        NearbyVisitor nearbyVisitor = new NearbyVisitor();
+        cat.target(Items.browning, nearbyVisitor);
         cat.shoot(nearbyVisitor);
         System.out.println(nearbyVisitor.fire());
         System.out.println(cat.look(Items.doortodinner));
         cat.toCatch(Items.primus, Location.underthearm);
         cat.location = Location.mantel;
         cat.location = Location.cornice;
-        FirstVisitor firstVisitor = new FirstVisitor("Вошедший", 100, Location.floor, Gender.human, Personality.calm);
+        FirstVisitor firstVisitor = new FirstVisitor();
         firstVisitor.toCatch(Items.curtain, Location.floor);
         System.out.println(cat.hill());
         cat.jumpto(cat);
         cat.toCatch(Items.browning, Location.chandelier);
         Items.primus.location = Location.chandelier;
         cat.target(Items.browning, firstVisitor);
-        SecondVisitor secondVisitor = new SecondVisitor("еще Вошедший",100,Location.floor, Gender.human, Personality.calm);
+        SecondVisitor secondVisitor = new SecondVisitor();
         cat.shoot(secondVisitor);
-        firstVisitor.target(Items.mauser,cat);
-        secondVisitor.target(Items.mauser,cat);
+        firstVisitor.target(Items.mauser, cat);
+        secondVisitor.target(Items.mauser, cat);
         firstVisitor.shoot(cat);
+
 
     }
 }
