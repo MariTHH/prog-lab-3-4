a9114ded446361bc448ef80b894cd3f0aff48b8c
MariTHH <118300117+MariTHH@users.noreply.github.com>
2023-01-15 12:16:18 +0300

    Update Thrower.java

--- src/Thrower.java
+++ src/Thrower.java
@@ -1,22 +1,24 @@
-import java.util.*;
-public class Thrower extends Character implements drop{
 
-    public Thrower(String name, int health, Location location, Gender gender, Personality personality) {
-        super(name, health, location, gender, personality);
+
+public class Thrower extends Character implements drop {
+
+    public Thrower() {
+        super("Бросавший", 100, Location.floor, Gender.human, Personality.calm);
     }
+
     @Override
-    public void drop(Items thing, Character cat){
-        if (thing==Items.net & location==Location.mantel) {
+    public void drop(Items thing, Character cat) {
+        if (thing == Items.net & location == Location.mantel) {
             cat.personality = personality.caught;
-        } else if (thing==Items.net & location!=Location.mantel){
+        } else if (thing == Items.net & location != Location.mantel) {
             cat.personality = personality.calm;
-            this.personality=personality.surprised;
-            System.out.println(name+ " " + this.personality.getNamePersonality() + " промахнулся ");
+            this.personality = personality.surprised;
+            System.out.println(name + " " + this.personality.getNamePersonality() + " промахнулся ");
             Items.jug.getLocation(Location.floor);
             Items.jug.getPersonality(Personality.broken);
-            this.personality=personality.broken;
+            this.personality = personality.broken;
             Location.floor.getPersonality(Personality.load);
-            System.out.println(Items.jug.getNameItem()+ " "+ personality.getNamePersonality());
+            System.out.println(Items.jug.getNameItem() + " " + personality.getNamePersonality());
 
         }
     }
